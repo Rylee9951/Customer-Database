b3c76fcbaa11312c75a13f1a51f0d958b394f9cf
Ryan Lee <djrlee51@gmail.com>
2017-07-12 14:19:10 -0700

    final commit

--- main.js
+++ main.js
@@ -2,19 +2,15 @@ let htmlStr = ''
 
 customers.results.map(function(item){
 	htmlStr += `
-	<div>
-		<img src="${item.picture.large}"/>
-		<h2 class="name">${item.name.first}</h2><h2>${item.name.last}</h2>
+	<div class="contact">
+		<img class="image" src="${item.picture.large}"/>
+		<h2 class="name">${item.name.first} ${item.name.last}</h2>
 		<p class="email">${item.email}</p>
-		<p class="address">${item.location.street}${item.location.city}${item.location.state}${item.location.postcode}</p>
+		<p class="address">${item.location.street} ${item.location.city} ${item.location.state} ${item.location.postcode}</p>
 		<p class="mobile">${item.cell}</p>
 		<p class="social">${item.id.value}</p>
 	</div>
 	`
 })
- 
 
-
-
-
-document.querySelector('#container').innerHTML = htmlStr
+document.querySelector('#container').innerHTML += htmlStr
